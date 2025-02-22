72f45347c29d86d9664ecf4de7b6a73da464a094
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-02-22 13:04:15 +0300

    feat: add place and date

--- src/components/PlaceAndDate/PlaceAndDate.tsx
+++ src/components/PlaceAndDate/PlaceAndDate.tsx
@@ -1,9 +1,30 @@
 import styles from "./PlaceAndDate.module.css";
+import leftStar from "../../assets/star_full.svg";
+import rightStar from "../../assets/star_empty.svg";
+import weAreGladToSeeYou from "../../assets/wearegladtoseeyou.svg";
+import place from "../../assets/place.svg";
 
 const PlaceAndDate = () => {
   return (
-    <div>
-      <h1>PlaceAndDate</h1>
+    <div className={styles.container}>
+      <div className={styles.date}>
+        <p>13.09.2025</p>
+        <img src={leftStar} className={styles.left_star} />
+        <img src={rightStar} className={styles.right_star} />
+      </div>
+      <div className={styles.huba_buba_cont}>
+        <img src={weAreGladToSeeYou} />
+      </div>
+      <div className={styles.place_cont}>
+        <img src={place} />
+      </div>
+      <a
+        href="https://yandex.ru/maps/-/CHuVy-~o"
+        target="_blank"
+        className={styles.link}
+      >
+        посмотреть на карте
+      </a>
     </div>
   );
 };
