6aff81889f423c5b67deedc838eea7c32e575109
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-02-27 21:09:55 +0300

    feat: convert to vector (place and date)

--- src/components/PlaceAndDate/PlaceAndDate.tsx
+++ src/components/PlaceAndDate/PlaceAndDate.tsx
@@ -1,23 +1,10 @@
 import styles from "./PlaceAndDate.module.css";
-import leftStar from "../../assets/star_full.svg";
-import rightStar from "../../assets/star_empty.svg";
-import weAreGladToSeeYou from "../../assets/wearegladtoseeyou.svg";
-import place from "../../assets/place.svg";
+import placeAndDate from '../../assets/dateAndPlace.svg'
 
 const PlaceAndDate = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.date}>
-        <p>13.09.2025</p>
-        <img src={leftStar} className={styles.left_star} />
-        <img src={rightStar} className={styles.right_star} />
-      </div>
-      <div className={styles.huba_buba_cont}>
-        <img src={weAreGladToSeeYou} />
-      </div>
-      <div className={styles.place_cont}>
-        <img src={place} />
-      </div>
+        <img src={placeAndDate} />
       <a
         href="https://yandex.ru/maps/-/CHuVy-~o"
         target="_blank"
