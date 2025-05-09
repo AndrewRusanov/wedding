f1c079be4bae21327215793e741196b7fdc260c9
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-05-09 17:36:36 +0300

    fix: change images

--- src/components/FlowersChat/FlowersChat.tsx
+++ src/components/FlowersChat/FlowersChat.tsx
@@ -1,16 +1,24 @@
-import { FC } from 'react';
-import styles from './FlowersChat.module.css';
-import flowersChat from "../../assets/FlowersChat.svg"
+import { FC } from "react";
+import flowersChat from "../../assets/FlowersChat.svg";
+import bottle from "../../assets/Бутылка и рамка.png";
+import furschet from "../../assets/Фуршет и рамка.png";
+import styles from "./FlowersChat.module.css";
 
 const FlowersChat: FC = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.clean_container}>
       <img src={flowersChat} className={styles.place} />
+      <div className={styles.flowersContainer}>
+        <img src={bottle} className={styles.flowersExample} />
+        <img src={furschet} className={styles.flowersExample} />
+      </div>
+
       <a
         href="https://t.me/+R97-7slmd5Q3MmRi"
         target="_blank"
         className={styles.link}
-      > чат гостей
+      >
+        чат гостей
       </a>
     </div>
   );
